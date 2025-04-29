0a2eb09f6ed1d4b14fc74273ca48d4c5a0b441ff
Mac McMillan <mmcmillan@srspharmacy.com>
2025-04-29 10:52:20 -0400

    add basic contact info

--- src/app/(public)/contact-us/page.js
+++ src/app/(public)/contact-us/page.js
@@ -5,6 +5,21 @@ export default function ContactUs() {
   return (
     <Container maxWidth="xl">
       <Typography variant="h2">Contact Us!</Typography>
+      <Grid container spacing={1} flexDirection="row">
+        <Grid item size={12}>
+          <Typography variant="body1">
+            Have a custom resin idea?<br/>
+            Questions about sizes or pricing?<br/>
+            Reach out and see what we can do.
+          </Typography>
+        </Grid>
+        <Grid item size={12}>
+          <Typography variant="body1">
+            Phone: 989-293-7975<br/>
+            E-mail: <a href="mailto:theresinbunch@gmail.com">TheResinBunch@gmail.com</a>
+          </Typography>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
